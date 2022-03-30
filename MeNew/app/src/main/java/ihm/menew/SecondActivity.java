f1abf6cf1a786fe2d7bfbd761c1510a2e7c09963
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2022-03-30 17:26:58 +0200

    Fragement sur la page principale

--- MeNew/app/src/main/java/ihm/menew/SecondActivity.java
+++ MeNew/app/src/main/java/ihm/menew/SecondActivity.java
@@ -2,25 +2,22 @@ package ihm.menew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 
 import ihm.menew.fragments.MainFragment;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
+public class SecondActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
     @Override
     public void onButtonClicked(View view) {
         Log.e(getClass().getSimpleName(),"Button clicked !");
-        startActivity(new Intent(this, SecondActivity.class));
     }
 }
---
+++ MeNew/app/src/main/java/ihm/menew/fragments/MainFragment.java
@@ -0,0 +1,72 @@
+package ihm.menew.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ihm.menew.R;
+
+
+public class MainFragment extends Fragment implements View.OnClickListener {
+
+    //2 - Declare callback
+    private OnButtonClickedListener mCallback;
+
+    // 1 - Declare our interface that will be implemented by any container activity
+    public interface OnButtonClickedListener {
+        public void onButtonClicked(View view);
+    }
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout
+        View result = inflater.inflate(R.layout.fragment_main, container, false);
+        //Set onClickListener to button "SHOW ME DETAILS"
+        result.findViewById(R.id.fragment_main_button).setOnClickListener(this);
+
+        return result;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mCallback.onButtonClicked(v);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // 4 - Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
+    }
+
+    // --------------
+    // FRAGMENT SUPPORT
+    // --------------
+
+    // 3 - Create callback to parent activity
+    private void createCallbackToParentActivity(){
+        try {
+            //Parent activity will automatically subscribe to callback
+            mCallback = (OnButtonClickedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(e.toString()+ " must implement OnButtonClickedListener");
+        }
+    }
+
+}
